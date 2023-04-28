ea4bfa934a52e4889136cd87a950eaeda46dd370
Ivan Jutamulia <jutamuliaivan@gmail.com>
2023-04-28 10:28:42 -0400

    feat(scaffold): Remove CRA scaffold and check in graphics

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./assets/sportsleague_logo.png";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to SportsLeague!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Welcome to SportsLeague!</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
