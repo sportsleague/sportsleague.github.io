5ce503833975f109c116673c889c8653b685afc9
Ivan Jutamulia <jutamuliaivan@gmail.com>
2023-04-29 11:09:24 -0400

    feat(navbar): Front-end design of Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "./assets/sportsleague_logo.png";
 import "./App.css";
+import NavigationBar from "./NavigationBar";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <NavigationBar></NavigationBar>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Welcome to SportsLeague!</p>
---
+++ src/NavigationBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+
+import "./NavigationBar.css";
+
+import sportsleague_logo from "./assets/sportsleague_logo.png";
+
+class NavigationBar extends React.Component {
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Container fluid>
+          <Navbar.Brand href="#">
+            <img
+              alt=""
+              src={sportsleague_logo}
+              width="75"
+              height="75"
+              className="d-inline-block align-top"
+            />{" "}
+            <strong>SPORTSLEAGUE</strong>
+          </Navbar.Brand>
+
+          <Nav className="justify-content-center">
+            <Nav.Link href="#home">HOME</Nav.Link>
+            <Nav.Link href="#about">ABOUT</Nav.Link>
+            <Nav.Link href="#team">TEAM</Nav.Link>
+            <Nav.Link href="#contact">CONTACT</Nav.Link>
+          </Nav>
+
+          <Button variant="warning" size="lg">
+            <strong>SIGN UP</strong>
+          </Button>
+        </Container>
+      </Navbar>
+    );
+  }
+}
+
+export default NavigationBar;
