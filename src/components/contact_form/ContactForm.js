ece16e986d771e11d9f07dbcfcbc936c2e302e9c
Ivan Jutamulia <jutamuliaivan@gmail.com>
2023-05-06 16:33:28 -0400

    feat(contact): Improved contact form appearance and functionality

--- src/components/contact_form/ContactForm.js
+++ src/components/contact_form/ContactForm.js
@@ -4,43 +4,111 @@ import "./ContactForm.css";
 
 export default function ContactForm() {
   const [formStatus, setFormStatus] = useState("Send");
+  const [submitOpen, setSubmitOpen] = useState(true);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+  const [message, setMessage] = useState("");
+
   const onSubmit = (e) => {
     e.preventDefault();
+    setSubmitOpen(false);
     setFormStatus("Submitting...");
-    const { name, email, message } = e.target.elements;
+    const { name, email, subject, message } = e.target.elements;
     let conFom = {
       name: name.value,
       email: email.value,
+      subject: subject.value,
       message: message.value,
     };
     console.log(conFom);
+    setFormStatus("Message sent!");
+    onReset();
+    alert("Your message has been sent!");
+  };
+
+  const onReset = () => {
+    setFormStatus("Send");
+    setSubmitOpen(true);
+    setName("");
+    setEmail("");
+    setSubject("");
+    setMessage("");
   };
 
   return (
     <div className="container mt-5">
       <h2 className="mb-3">Contact Us!</h2>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit} onReset={onReset}>
         <div className="mb-3">
           <label className="form-label" htmlFor="name">
             Name
           </label>
-          <input className="form-control" type="text" id="name" required />
+          <input
+            className="form-control"
+            type="text"
+            id="name"
+            value={name}
+            required
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="email">
-            Email
+            Email Address
           </label>
-          <input className="form-control" type="email" id="email" required />
+          <input
+            className="form-control"
+            type="email"
+            id="email"
+            value={email}
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="subject">
+            Subject
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            id="subject"
+            value={subject}
+            required
+            onChange={(e) => setSubject(e.target.value)}
+          />
         </div>
         <div className="mb-3">
           <label className="form-label" htmlFor="message">
             Message
           </label>
-          <textarea className="form-control" id="message" required />
+          <textarea
+            className="form-control"
+            id="message"
+            value={message}
+            required
+            onChange={(e) => setMessage(e.target.value)}
+            rows="5"
+          />
         </div>
-        <button className="btn btn-danger" type="submit">
-          {formStatus}
-        </button>
+        <div className="buttons">
+          <button
+            className="btn btn-outline-danger"
+            id="reset-button"
+            type="reset"
+          >
+            Reset
+          </button>
+          <button
+            className="btn btn-disabled"
+            id="submit-button"
+            type="submit"
+            disabled={!submitOpen}
+          >
+            {formStatus}
+          </button>
+        </div>
       </form>
     </div>
   );
